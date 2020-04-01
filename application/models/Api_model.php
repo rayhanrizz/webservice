6c9a7ff813ef5f9786bf1d300eb6d3233c74193e
rayhanrizz <57081483+rayhanrizz@users.noreply.github.com>
2020-04-01 09:36:23 +0700

    first commit

---
+++ application/models/Api_model.php
@@ -0,0 +1,16 @@
+<?php
+class Api_model extends CI_model
+{
+	function get_all()
+	{
+		$this->db->order_by('id_barang','DESC');
+		return $this->db->get('barang');
+	}
+	function get_by_id($id)
+	{
+		$this->db->where('id_barang',$id);
+		$query = $this->db->get('barang');
+		return $query->result_array();
+	}
+}
+?>
